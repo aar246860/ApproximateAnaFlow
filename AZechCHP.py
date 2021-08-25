08e6a74bae040f5797afa6b77b94fd2f2f589d8b
aar246860 <89235885+aar246860@users.noreply.github.com>
2021-08-25 11:45:01 +0800

    Update AZechCHP.py

--- AZechCHP.py
+++ AZechCHP.py
@@ -1,25 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
-from anaflow import theis, ext_theis_2d
 import scipy.special as sc
 import numpy as np
 from scipy.special import expi
 from mpmath import *
 mp.dps = 10; mp.prec = 60; mp.pretty = True
-import matplotlib.pyplot as plt
-import pandas as pd
-from scipy.optimize import curve_fit
-from scipy.optimize import differential_evolution
 import warnings
 
-
-# In[24]:
-
-
+#for constant-head pumpint#
 def app_ext_CHP_2d(time, rad, S, TG, var, Len_scale, s0):
     zeta = 1.6
     rw = 0.05
@@ -36,6 +22,7 @@ def app_ext_CHP_2d(time, rad, S, TG, var, Len_scale, s0):
     sp = s0*(GammaR-Gamma)/(GammaR-Gammarw)
     return sp
 
+#for wellbore flowrate#
 def Flow(time, S, TG, var, Len_scale, s0):
     zeta = 1.6
     rw = 0.05
@@ -51,62 +38,3 @@ def Flow(time, S, TG, var, Len_scale, s0):
     Gammarw = np.exp(var/2)*expi(lamda1rw)-expi(lamda2rw)
     sp = s0*4*np.pi*TG/(GammaR-Gammarw)
     return sp
-
-
-# In[28]:
-
-
-t1=np.logspace(0,4,num=100,base=10)
-#T value
-label_ef = "extended Theis"
-label_af = "apprimate extended Theis"
-TT1=[];
-TT1=[];
-TT2=[];
-TT3=[];
-TT4=[];
-TT5=[];
-for n in range(0, len(t1)):
-    TT1.append(app_ext_CHP_2d(t1[n], 0.05, 0.0001, 0.0001, 1, 1, 1))
-    TT2.append(app_ext_CHP_2d(t1[n], 0.05, 0.0001, 0.0001, 1, 2, 1))
-    TT3.append(app_ext_CHP_2d(t1[n], 0.05, 0.0001, 0.0001, 1, 3, 1))
-    TT4.append(app_ext_CHP_2d(t1[n], 0.05, 0.0001, 0.0001, 1, 4, 1))
-    TT5.append(app_ext_CHP_2d(t1[n], 0.05, 0.0001, 0.0001, 1, 5, 1))
-plt.plot(t1, TT1, label=label_ef, color="C"+str(1))
-plt.plot(t1, TT2, label=label_ef, color="C"+str(2))
-plt.plot(t1, TT3, label=label_ef, color="C"+str(3))
-plt.plot(t1, TT4, label=label_ef, color="C"+str(4))
-plt.plot(t1, TT5, label=label_ef, color="C"+str(5))
-
-
-# In[18]:
-
-
-t1=np.logspace(0,4,num=100,base=10)
-#T value
-label_ef = "extended Theis"
-label_af = "apprimate extended Theis"
-TT1=[];
-TT1=[];
-TT2=[];
-TT3=[];
-TT4=[];
-TT5=[];
-for n in range(0, len(t1)):
-    TT1.append(Flow(t1[n], 0.0001, 0.0001, 1, 1, 1))
-    TT2.append(Flow(t1[n], 0.0001, 0.0001, 1, 2, 1))
-    TT3.append(Flow(t1[n], 0.0001, 0.0001, 1, 3, 1))
-    TT4.append(Flow(t1[n], 0.0001, 0.0001, 1, 4, 1))
-    TT5.append(Flow(t1[n], 0.0001, 0.0001, 1, 5, 1))
-plt.plot(t1, TT1, label=label_ef, color="C"+str(1))
-plt.plot(t1, TT2, label=label_ef, color="C"+str(2))
-plt.plot(t1, TT3, label=label_ef, color="C"+str(3))
-plt.plot(t1, TT4, label=label_ef, color="C"+str(4))
-plt.plot(t1, TT5, label=label_ef, color="C"+str(5))
-
-
-# In[ ]:
-
-
-
-
